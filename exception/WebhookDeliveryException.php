e61d5396da5ff64c82b18483bc0ef7eb25977905
ovchinnikov_av <ovchinnikov_av>
2024-07-07 09:53:14 +0300

    Добавил комментарии;

--- exception/WebhookDeliveryException.php
+++ exception/WebhookDeliveryException.php
@@ -2,8 +2,16 @@
 
 declare(strict_types=1);
 
-namespace exeption;
+namespace exception;
 
+/**
+ * Class WebhookDeliveryException
+ *
+ * This class represents an exception that occurs when there is a problem delivering a webhook.
+ * It extends the built-in \RuntimeException class.
+ */
 class WebhookDeliveryException extends \RuntimeException
 {
+    // The class does not contain any additional methods or properties,
+    // as it is used solely for indicating that a webhook delivery failure has occurred.
 }
